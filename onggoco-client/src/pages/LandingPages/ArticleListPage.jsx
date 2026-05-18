b7402c2ae013836f2470d1cda9775920765986e1
ellicaonggoco <ellicaonggoco19@gmail.com>
2026-05-19 05:34:34 +0800

    Fix Button import path in ArticleListPage

--- onggoco-client/src/pages/LandingPages/ArticleListPage.jsx
+++ onggoco-client/src/pages/LandingPages/ArticleListPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Button from "./Button";
+import Button from "../../components/Button";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
